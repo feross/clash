0ceeaa064fa68bfac5820c6f9711a0f058ad7087
Jake McKinnon <jmckinnon99@gmail.com>
2019-03-09 13:39:38 -0800

    basic case match > & <

--- src/job-parser.cc
+++ src/job-parser.cc
@@ -3,13 +3,42 @@
 ParsedJob JobParser::Parse(string& job_str) {
     ParsedJob job;
 
-    vector<string> pipeline_strs = StringUtil::Split(job_str, ";");
+    vector<string> pipeline_strs = StringUtil::Split(job_str, ";"); //TODO: fix, maybe escaped ('\")
     for (string& pipeline_str : pipeline_strs) {
         ParsedPipeline pipeline;
 
-        vector<string> command_strs = StringUtil::Split(pipeline_str, "|");
+        vector<string> command_strs = StringUtil::Split(pipeline_str, "|"); //TODO: fix, maybe escaped ('\")
         for (string& command_str : command_strs) {
             ParsedCommand command;
+            // printf("%s\n", command_str.c_str());
+
+            vector<string> split_command = StringUtil::Split(command_str, ">"); //TODO: fix, maybe escaped ('\")
+            command_str = split_command[0];
+            if (split_command.size() >= 2) {
+                for (int i = 1; i < split_command.size(); i++) {
+                    int start_pos = split_command[i].find_first_not_of(' ');
+                    int end_pos = split_command[i].find_first_of(' ', start_pos);
+                    command.outputFile = split_command[i].substr(start_pos, end_pos);
+                    if (end_pos < split_command[i].size()) {
+                        command_str.push_back(' ');
+                        command_str.append(split_command[i].substr(end_pos));
+                    }
+                }
+            }
+
+            split_command = StringUtil::Split(command_str, "<"); //TODO: fix, maybe escaped ('\")
+            command_str = split_command[0];
+            if (split_command.size() >= 2) {
+                for (int i = 1; i < split_command.size(); i++) {
+                    int start_pos = split_command[i].find_first_not_of(' ');
+                    int end_pos = split_command[i].find_first_of(' ', start_pos);
+                    command.outputFile = split_command[i].substr(start_pos, end_pos);
+                    if (end_pos < split_command[i].size()) {
+                        command_str.push_back(' ');
+                        command_str.append(split_command[i].substr(end_pos));
+                    }
+                }
+            }
 
             // TODO: handle multiple whitespaces in a row, different whitespace
             //       types
